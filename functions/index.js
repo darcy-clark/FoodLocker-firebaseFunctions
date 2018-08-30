0b5633a216511eccf49c2a18f0445a595f255bc0
Darcy Clark <drgclark@edu.uwaterloo.ca>
2018-08-29 21:39:40 -0400

    Functional onOrderCreate function to give volunteers notification of order

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.onNewOrder = functions.database.ref('/orders/{id}')
+    .onCreate((snapshot, context) => {
+      console.log('New Order!');
+
+      var message = {
+        notification: {
+          title: 'Welcome!',
+          body: 'You are Mexican'
+        },
+        topic: "volunteer"
+      };
+
+      return admin.messaging().send(message)
+          .then((response) => {
+            console.log('Notification has been sent');
+            return response;
+          })
+          .catch((error) => {
+            console.log('We encountered an error');
+            console.log(error);
+          });
+    });
+
